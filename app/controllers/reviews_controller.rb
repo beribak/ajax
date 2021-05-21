49820d9d36b8deb21c93e9f2613a2e534593482b
germain loret <germain.loret@gmail.com>
2021-05-21 11:05:32 +0200

    Ajax in rails with render json

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,10 +3,15 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-    if @review.save
-      redirect_to restaurant_path(@restaurant)
-    else
-      render 'restaurants/show'
+
+    respond_to do |format|
+      if @review.save
+        format.html { redirect_to restaurant_path(@restaurant, anchor: "review-#{@review.id}") }
+        format.json { render json: success_data }
+      else
+        format.html { render 'restaurants/show' }
+        format.json { render json: unprocessable_data, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -14,5 +19,18 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:content)
+  end
+
+  def success_data
+    {
+      inserted_item: render_to_string(partial: 'restaurants/review.html', locals: { review: @review }),
+      form: render_to_string(partial: 'reviews/form.html', locals: { restaurant: @restaurant, review: Review.new })
+    }
+  end
+
+  def unprocessable_data
+    {
+      form: render_to_string(partial: 'reviews/form.html', locals: { restaurant: @restaurant, review: @review })
+    }
   end
 end
