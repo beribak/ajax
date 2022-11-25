b2e76087b57c955734bf4be7e729914405d50ffc
danko beribak <beribakdanko@gmail.com>
2022-11-25 22:59:31 +0100

    first commit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,14 +3,16 @@ class ReviewsController < ApplicationController
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = Review.new(review_params)
     @review.restaurant = @restaurant
-
+  
     respond_to do |format|
       if @review.save
         format.html { redirect_to restaurant_path(@restaurant) }
-        format.json
+        format.text { render partial: "reviews/review", locals: {review: @review}, formats: [:html] }
+        # format.json
       else
         format.html { render "restaurants/show", status: :unprocessable_entity }
-        format.json
+        format.text { render partial: "reviews/review", locals: {review:  @review}, formats: [:html] }
+        # format.json  # if you want a json response
       end
     end
   end
--- app/javascript/controllers/insert_in_list_controller.js
+++ app/javascript/controllers/insert_in_list_controller.js
@@ -4,19 +4,32 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ["items", "form"]
 
+  connect() {
+  }
+  
   send(event) {
+    console.log(this.formTarget.action)
     event.preventDefault()
+
     fetch(this.formTarget.action, {
       method: "POST",
-      headers: {"Accept": "application/json"},
+      headers: {"Accept": "text/plain"}, // with JSON - headers: {"Accept": "application/json"},
       body: new FormData(this.formTarget)
     })
-      .then(response => response.json())
-      .then((data) => {
-        if (data.inserted_item) {
-          this.itemsTarget.insertAdjacentHTML("beforeend", data.inserted_item)
-        }
-        this.formTarget.outerHTML = data.form
-      })
+    .then(response => response.text()) // with JSON - .then(response => response.text())
+    .then((data) => {
+      
+      // with text
+      console.log(data)
+      this.itemsTarget.insertAdjacentHTML('beforeend', data)
+      this.formTarget.reset()
+
+      //with JSON
+      
+      // if (data.inserted_item) {
+      //   this.itemsTarget.insertAdjacentHTML("beforeend", data.inserted_item)
+      // }
+      // this.formTarget.outerHTML = data.form
+    })
   }
 }
